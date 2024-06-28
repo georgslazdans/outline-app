df3f0f82af82f6792bc47ef8127a1b7b3ca20124
Georgs Lazdāns <g.lazdans@gmail.com>
2024-06-28 10:52:59 +0300

    Add export output nextjs setting

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { default as PWA } from "next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
