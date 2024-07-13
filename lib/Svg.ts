319039f380c275f6d4acb3a3f0e5b3d31178f37f
Georgs Lazdāns <g.lazdans@gmail.com>
2024-07-13 11:17:31 +0300

    Make SVG export a single path

--- lib/Svg.ts
+++ lib/Svg.ts
@@ -2,20 +2,24 @@ import Point from "./Point";
 import { ContourPoints } from "./opencv/StepResult";
 
 namespace Svg {
-  export const from = (points: ContourPoints[]) => {
-    const pathData = points.map((it) => pathDataOf(it.points)).join(" ");
+  export const from = (contours: ContourPoints[]) => {
+    const pathData = pathDataOf(contours);
     return `<svg xmlns="http://www.w3.org/2000/svg">${pathData}</svg>`;
   };
 
-  const pathDataOf = (points: Point[]) => {
+  const pathDataOf = (contours: ContourPoints[]) => {
+    const pointData = contours.map(it=> pointDataOf(it.points)).join("");
+    return `<path d="${pointData}" stroke-width="1" stroke="black" fill="none"/>`;
+  };
+
+  const pointDataOf = (points: Point[]) => {
     let pathData = `M ${points[0].x} ${points[0].y}`;
     for (let i = 1; i < points.length; i++) {
       pathData += ` L ${points[i].x} ${points[i].y}`;
     }
     pathData += " Z"; // Close the path
-
-    return `<path d="${pathData}" stroke-width="1" stroke="black" fill="none"/>`;
-  };
+    return pathData;
+  }
 
   const maxSize = () => {};
 }
