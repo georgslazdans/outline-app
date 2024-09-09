7abfce4154761983a5f39ba130fabbd85575304d
Georgs Lazdāns <g.lazdans@gmail.com>
2024-09-09 17:55:21 +0300

    Add basic autosave

--- context/ModelContext.tsx
+++ context/ModelContext.tsx
@@ -1,8 +1,17 @@
 "use client";
-import React, { createContext, useContext, useState, ReactNode } from "react";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+  useCallback,
+  useRef,
+} from "react";
 import Model from "@/lib/Model";
 import { defaultGridfinityParams } from "@/lib/replicad/model/item/GridfinityParams";
 import { gridfinityItemOf } from "@/lib/replicad/model/item/Gridfinity";
+import { useIndexedDB } from "react-indexed-db-hook";
 type ModelContextType = {
   model: Model;
   setModel: React.Dispatch<React.SetStateAction<Model>>;
@@ -11,12 +20,50 @@ type ModelContextType = {
 const ModelContext = createContext<ModelContextType | undefined>(undefined);
 
 export const ModelProvider = ({ children }: { children: ReactNode }) => {
+  const { update } = useIndexedDB("models");
+
   const defaultModel = {
     name: "Untitled",
     modelData: { items: [gridfinityItemOf(defaultGridfinityParams())] },
     addDate: new Date(),
   };
-  const [model, setModel] = useState<Model>(defaultModel);
+  const [model, setModelContext] = useState<Model>(defaultModel);
+  const modelRef = useRef<Model>(model);
+
+  const doAutosave = useCallback(() => {
+    if (modelRef.current.id) {
+      update(modelRef.current).then(
+        () => {
+          console.log("Model saved!", modelRef.current.id);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
+  }, [update]);
+
+  const createAutosaveTimeout = useCallback(() => {
+    setTimeout(() => {
+      doAutosave();
+      createAutosaveTimeout();
+    }, 1000 * 30);
+  }, [doAutosave]);
+
+  useEffect(() => {
+    createAutosaveTimeout();
+  }, [createAutosaveTimeout]);
+
+  const setModel = (newModel: Model | ((model: Model) => Model)) => {
+    let data: Model;
+    if (typeof newModel === "function") {
+      data = newModel(model);
+    } else {
+      data = newModel;
+    }
+    setModelContext(data);
+    modelRef.current = data;
+  };
 
   return (
     <ModelContext.Provider
