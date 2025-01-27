74d8c09905f519f8dfd39a7a11fe37aec881a899
Georgs Lazdāns <g.lazdans@gmail.com>
2025-01-27 14:12:30 +0200

    Filter similar outlines in find paper step

--- lib/opencv/processor/steps/FindPaperOutline.ts
+++ lib/opencv/processor/steps/FindPaperOutline.ts
@@ -7,10 +7,19 @@ import ProcessingStep, {
 import ColorSpace from "../../util/ColorSpace";
 import { paperContoursOf } from "../../util/contours/Contours";
 import StepName from "./StepName";
-import { pointsFrom } from "@/lib/data/contour/ContourPoints";
+import ContourPoints, { pointsFrom } from "@/lib/data/contour/ContourPoints";
 import { drawAllContours } from "../../util/contours/Drawing";
 
-type FindPaperOutlineSettings = {};
+interface ContourResult {
+  index: number;
+  area: number;
+  points: ContourPoints;
+}
+
+type FindPaperOutlineSettings = {
+  filterSimilarOutlines: boolean;
+  similarityThreshold: number;
+};
 
 const PAPER_NOT_FOUND_MESSAGE =
   'Paper contours not found! Ensure that the paper outline is fully visible and uninterrupted in "Adaptive Threshold" step!';
@@ -24,32 +33,35 @@ const findPaperOutline: Process<FindPaperOutlineSettings> = (
 
   const contours = imageContours.contours;
 
-  const possibleOutlineIndexes = findPaperOutlineIndexes(
+  const possibleOutlines = findPaperOutlines(
     contours,
     paperOutlineThreshold(image)
   );
 
-  if (possibleOutlineIndexes.length == 0) {
+  if (possibleOutlines.length == 0) {
     return { errorMessage: PAPER_NOT_FOUND_MESSAGE };
   }
 
-  const outlinePoints = possibleOutlineIndexes.map((index) => {
-    const contour = contours.get(index);
-    const smoothedContour = smoothContour(contour);
-    const points = pointsFrom(smoothedContour);
-    smoothedContour.delete();
-    return points;
-  });
+  const filteredOutlines = settings.filterSimilarOutlines
+    ? filterOutlines(
+        possibleOutlines,
+        image.size(),
+        settings.similarityThreshold
+      )
+    : possibleOutlines;
 
   const result = drawAllContours(
     image.size(),
     imageContours,
-    possibleOutlineIndexes
+    filteredOutlines.map((it) => it.index)
   );
 
   imageContours.delete();
 
-  return { image: result, contours: outlinePoints };
+  const resultPoints = filteredOutlines
+    .sort((a, b) => (a.area > b.area ? -1 : 1))
+    .map((it) => it.points);
+  return { image: result, contours: resultPoints };
 };
 
 const paperOutlineThreshold = (image: cv.Mat) => {
@@ -58,10 +70,10 @@ const paperOutlineThreshold = (image: cv.Mat) => {
   return width * height * paperAreaThreshold;
 };
 
-const findPaperOutlineIndexes = (
+const findPaperOutlines = (
   contours: cv.MatVector,
   paperAreaThreshold: number
-): number[] => {
+): ContourResult[] => {
   const result = [];
   for (let i = 0; i < contours.size(); ++i) {
     const contour = contours.get(i);
@@ -70,7 +82,11 @@ const findPaperOutlineIndexes = (
       if (calculatedArea > paperAreaThreshold) {
         const smoothedContour = smoothContour(contour);
         if (smoothedContour.rows == 4) {
-          result.push(i);
+          result.push({
+            index: i,
+            area: calculatedArea,
+            points: pointsFrom(smoothedContour),
+          });
         }
         if (smoothedContour.rows > 4) {
           console.warn("Contour with more than 4 points found!");
@@ -82,6 +98,49 @@ const findPaperOutlineIndexes = (
   return result;
 };
 
+const filterOutlines = (
+  outlines: ContourResult[],
+  imageSize: cv.Size,
+  similarityThreshold: number = 0.01
+): ContourResult[] => {
+  const { width, height } = imageSize;
+  const distanceThreshold = Math.max(width, height) * similarityThreshold;
+  const areaThreshold = width * height * similarityThreshold;
+  
+  const result: ContourResult[] = [];
+
+  outlines.forEach((it) => {
+    const similarOutlines = outlines
+      .filter(
+        (outline) =>
+          Math.abs(outline.points.points[0].x - it.points.points[0].x) <
+            distanceThreshold &&
+          Math.abs(outline.points.points[0].y - it.points.points[0].y) <
+            distanceThreshold
+      )
+      .filter((outline) => Math.abs(outline.area - it.area) < areaThreshold);
+    if (similarOutlines.length != 0) {
+      const smallestOutline = smallestAreaOf(similarOutlines);
+      if (!result.includes(smallestOutline)) {
+        result.push(smallestOutline);
+      }
+    } else {
+      result.push(it);
+    }
+  });
+  return result;
+};
+
+const smallestAreaOf = (contours: ContourResult[]): ContourResult => {
+  let smallestContour = contours[0];
+  contours.forEach((it) => {
+    if (it.area < smallestContour.area) {
+      smallestContour = it;
+    }
+  });
+  return smallestContour;
+};
+
 const smoothContour = (contour: cv.Mat) => {
   let result = new cv.Mat();
   const accuracy = 0.02 * cv.arcLength(contour, true);
@@ -91,8 +150,23 @@ const smoothContour = (contour: cv.Mat) => {
 
 const findPaperOutlineStep: ProcessingStep<FindPaperOutlineSettings> = {
   name: StepName.FIND_PAPER_OUTLINE,
-  settings: {},
-  config: {},
+  settings: {
+    filterSimilarOutlines: true,
+    similarityThreshold: 0.01,
+  },
+  config: {
+    filterSimilarOutlines: {
+      type: "checkbox",
+      tooltip: "Filters similar outlines returning the smallest one",
+    },
+    similarityThreshold: {
+      type: "number",
+      min: 0,
+      max: 1,
+      step: 0.01,
+      tooltip: "Similarity threshold in percentage based on image pixel count",
+    },
+  },
   imageColorSpace: () => ColorSpace.GRAY_SCALE,
   process: findPaperOutline,
 };
