bc8f77affe30184a6fa303f6c9f0ba7f632b6cd8
Georgs Lazdāns <g.lazdans@gmail.com>
2025-01-29 08:31:30 +0200

    Download multiple contours as zip file

--- lib/svg/DownloadSvg.ts
+++ lib/svg/DownloadSvg.ts
@@ -1,25 +1,46 @@
-import {
+import ContourPoints, {
   ContourOutline,
   modifyContourList,
 } from "../data/contour/ContourPoints";
 import { downloadFile } from "../utils/Download";
 import Svg from "./Svg";
 import { PaperDimensions } from "../opencv/PaperSettings";
+import JsZip from "jszip";
 
 export const downloadAsSvg = (
   contourOutlines: ContourOutline[],
   paperDimensions: PaperDimensions
 ) => {
-  const contourPoints = contourOutlines.map((it) => {
-    const holes = it.holes ? it.holes : [];
-    return [it.outline, ...holes];
-  });
-  for (const points of contourPoints) {
-    const contours = modifyContourList(points).centerPoints(paperDimensions);
-    const svg = Svg.from(contours, paperDimensions);
-    const blob = new Blob([svg], {
-      type: "image/svg+xml",
-    });
+  if (contourOutlines.length == 1) {
+    const blob = asXmlFile(contourOutlines[0], paperDimensions);
     downloadFile(blob, `outline-${new Date().toLocaleDateString("lv")}.svg`);
+  } else {
+    const blobs = contourOutlines.map((it) => asXmlFile(it, paperDimensions));
+    asZipFile(blobs).then((zip) =>
+      downloadFile(zip, `outline-${new Date().toLocaleDateString("lv")}.zip`)
+    );
   }
+};
+
+const asXmlFile = (
+  outline: ContourOutline,
+  paperDimensions: PaperDimensions
+) => {
+  const points = pointsOf(outline);
+  const contours = modifyContourList(points).centerPoints(paperDimensions);
+  const svg = Svg.from(contours, paperDimensions);
+  return new Blob([svg], {
+    type: "image/svg+xml",
+  });
+};
+
+const pointsOf = (outline: ContourOutline): ContourPoints[] => {
+  const holes = outline.holes ? outline.holes : [];
+  return [outline.outline, ...holes];
+};
+
+const asZipFile = (blobs: Blob[]): Promise<Blob> => {
+  var zip = new JsZip();
+  blobs.forEach((blob, index) => zip.file(`outline-${index}.svg`, blob));
+  return zip.generateAsync({ type: "blob" });
 };
