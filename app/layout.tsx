72cd2d457d08982adaad88d771fb3c5325c010ed
Georgs Lazdāns <g.lazdans@gmail.com>
2025-02-07 08:34:04 +0200

    Add loading indicator when processing IndexedDB migration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,47 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 import "./globals.css";
-import {LoadingProvider} from "@/context/LoadingContext";
-import {getDictionary} from "./dictionaries";
+import { LoadingProvider } from "@/context/LoadingContext";
+import { getDictionary } from "./dictionaries";
 import dynamic from "next/dynamic";
 import Navbar from "@/components/navbar/Navbar";
-import {ErrorProvider} from "@/components/error/ErrorContext";
+import { ErrorProvider } from "@/components/error/ErrorContext";
 
 const IndexedDbContext = dynamic(() => import("@/db/IndexedDbContext"), {
-    ssr: false,
+  ssr: false,
 });
 const DetailsProvider = dynamic(() => import("@/context/DetailsContext"), {
-    ssr: false,
+  ssr: false,
 });
 
 export const metadata: Metadata = {
-    title: "Outline App",
-    description: "Create SVG paths from images",
-    manifest: "/manifest.json",
+  title: "Outline App",
+  description: "Create SVG paths from images",
+  manifest: "/manifest.json",
 };
 
 export default async function RootLayout({
-                                             children,
-                                         }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    const dictionary = await getDictionary("en");
+  const dictionary = await getDictionary("en");
 
-    return (
-        <html lang="en">
-        <body>
-        <Navbar dictionary={dictionary}/>
+  return (
+    <html lang="en">
+      <body>
+        <Navbar dictionary={dictionary} />
         <main className="flex min-h-full flex-col items-center justify-between p-4">
-            <div className="z-10 w-full max-w-5xl items-center justify-between">
-                <IndexedDbContext></IndexedDbContext>
-                <ErrorProvider dictionary={dictionary}>
-                    <LoadingProvider dictionary={dictionary}>
-                        <DetailsProvider>
-                            {children}
-                        </DetailsProvider>
-                    </LoadingProvider>
-                </ErrorProvider>
-            </div>
+          <div className="z-10 w-full max-w-5xl items-center justify-between">
+            <LoadingProvider dictionary={dictionary}>
+              <IndexedDbContext></IndexedDbContext>
+
+              <ErrorProvider dictionary={dictionary}>
+                <DetailsProvider>{children}</DetailsProvider>
+              </ErrorProvider>
+            </LoadingProvider>
+          </div>
         </main>
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
--- db/IndexedDbContext.tsx
+++ db/IndexedDbContext.tsx
@@ -3,13 +3,41 @@
 import { DBConfig } from "@/db/DbConfig";
 import { initDB as initDBHooks } from "react-indexed-db-hook";
 import { initializeAndMigrateDb } from "./InitializeDb";
+import { useLoading } from "@/context/LoadingContext";
+import { useEffect, useState } from "react";
+import useDebounced from "@/lib/utils/Debounced";
 
-if (window) {
-  initializeAndMigrateDb(DBConfig);
-  initDBHooks(DBConfig);
-}
+const initializeDb = (async () => {
+  if (window) {
+    const migration = initializeAndMigrateDb(DBConfig);
+    initDBHooks(DBConfig);
+    await migration;
+  }
+})();
 
 const IndexedDbContext = () => {
+  const { setLoading } = useLoading();
+  const [isInitialized, setIsInitialized] = useState(false);
+  const { onChange: setLoadingDebounced } = useDebounced(setLoading, 200);
+
+  useEffect(() => {
+    setLoadingDebounced(true);
+  }, []);
+
+  useEffect(() => {
+    if (isInitialized) {
+      setLoadingDebounced(false);
+    }
+  }, [isInitialized]);
+
+  useEffect(() => {
+    const init = async () => {
+      await initializeDb;
+      setIsInitialized(true);
+    };
+    init();
+  }, []);
+
   return <></>;
 };
 
