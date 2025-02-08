72c3ff4903a2ba4e1df717689bd034b2b66acd67
Georgs Lazdāns <g.lazdans@gmail.com>
2025-02-08 09:36:06 +0200

    Always show close icon when scrolling

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -13,15 +13,19 @@ const Modal = ({ isOpen, onClose, children }: Props) => {
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div
         className="bg-white dark:bg-black rounded-lg shadow-lg 
-        w-full max-w-lg mx-auto p-6 relative max-h-[75vh] xl:max-h-[85vh] overflow-auto"
+        w-full max-w-lg mx-auto relative"
       >
-        <button
-          className="absolute top-2 right-2 text-gray-600 w-8 h-8 text-3xl"
-          onClick={onClose}
-        >
-          &#x2715; {/* Close Icon */}
-        </button>
-        {children}
+        <div>
+          <button
+            className="absolute top-2 right-2.5 text-gray-600 w-8 h-8 text-3xl"
+            onClick={onClose}
+          >
+            &#x2715; {/* Close Icon */}
+          </button>
+        </div>
+        <div className=" p-6 overflow-auto h-full max-h-[75vh] xl:max-h-[85vh]">
+          {children}
+        </div>
       </div>
     </div>
   );
