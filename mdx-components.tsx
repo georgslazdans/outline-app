32e566f7b9a4bae4c91155568e8cb3b7a9b3acf7
Flaviu Tamas <me@flaviutamas.com>
2025-11-16 14:46:25 -0500

    Remove unusual spacing in headers
    I don't think I've seen a website before that has one of the headers
    indented a little bit. I've removed the extra left spacing to make the
    text more consistent.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -5,10 +5,10 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     h1: ({ children }) => (
       <h1 className="text-center p-2 mt-2 mb-6">{children}</h1>
     ),
-    h2: ({ children }) => <h2 className="mt-8 mb-2 ml-4"> {children}</h2>,
-    h3: ({ children }) => <h3 className="mt-2 mb-1"> {children}</h3>,
-    h4: ({ children }) => <h4 className="mb-1"> {children}</h4>,
-    p: ({ children }) => <p className="mb-1"> {children}</p>,
+    h2: ({ children }) => <h2 className="mt-8 mb-2">{children}</h2>,
+    h3: ({ children }) => <h3 className="mt-2 mb-1">{children}</h3>,
+    h4: ({ children }) => <h4 className="mb-1">{children}</h4>,
+    p: ({ children }) => <p className="mb-1">{children}</p>,
     a: ({ children, href }) => (
       <a style={{ color: "revert", textDecoration: "revert" }} href={href}>
         {children}
